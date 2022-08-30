5034b642551cb47c65dcb994368edbd7ada3a21e
NKLam <khoalamvn@gmail.com>
2022-08-30 16:44:42 +1000

    Init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Giang Project
+
+## Command
+
+Required Anaconda or Miniconda installed.
+
+Create a new virtual environment and install packages
+
+```python
+conda env create -f environment.yml
+```
+
+Update environment
+
+```python
+conda env update -f environment.yml --prune
+```
+
+To run
+
+```python
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,24 @@
+from imghdr import what
+from PyPDF2 import PdfReader, PdfWriter
+
+reader = PdfReader("form.pdf")
+writer = PdfWriter()
+
+page = reader.pages[0]
+fields = reader.get_fields()
+
+whatever = reader.pages[0].getContents()
+print(whatever)
+
+for field in fields:
+    print(field)
+
+writer.add_page(page)
+
+writer.update_page_form_field_values(
+    writer.pages[0], {"fieldname": "some filled in text"}
+)
+
+# write "output" to PyPDF2-output.pdf
+with open("filled-out.pdf", "wb") as output_stream:
+    writer.write(output_stream)
